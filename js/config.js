242707544ab9a0a37f0f9265377aa3cf69244f0f
vm0isip8gthb <vincent.isip@gmail.com>
2019-05-10 19:56:56 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_BDHaUw1wD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'hmd7tqde9fokh0bb7lbieoa0b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
